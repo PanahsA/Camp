c1d5216795c262cc9e9c840116e87af615f0a674
PanahsA <ebri_bmg@hotmail.com>
2019-07-27 02:55:35 +0300

    trying

--- app.js
+++ app.js
@@ -61,12 +61,12 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 //====================
 //SERVER LISTENING
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//     port = 8000;
-// }
-// app.listen(port);
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port);
 
-app.listen(3000,function () {
-    console.log("Server is up");
-});
+// app.listen(3000,function () {
+//     console.log("Server is up");
+// });
