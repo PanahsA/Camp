38b28c36641a4f21ed498d8eb01df1af8b9416c1
PanahsA <ebri_bmg@hotmail.com>
2019-07-27 02:39:23 +0300

    correct port

--- app.js
+++ app.js
@@ -61,5 +61,8 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 //====================
 //SERVER LISTENING
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
 app.listen(port);
-
