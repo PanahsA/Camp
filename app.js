2eaf401a7d17a92163d547bb70cbb767213ab8f4
PanahsA <ebri_bmg@hotmail.com>
2019-07-28 20:06:13 +0300

    3000

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 //====================
 
 
-let port = 8000;
+let port = process.env.PORT;
 if (port == null || port == "") {
     port = 8000;
 }
