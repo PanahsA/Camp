6227673d0e74095f4a352706447901406e177e49
PanahsA <ebri_bmg@hotmail.com>
2019-08-09 21:13:50 +0300

    Uses positions corrected

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 //Requirements
-var express        =require("express");
-var app            =express();
-var mongoose       =require("mongoose");
-var bodyParser     =require("body-parser");
-var Comment        =require("./models/comment");
-var Campground     =require("./models/campground");
-var User           =require("./models/user");
-var seedDB         =require("./seeds");
-var passport       =require("passport");
-var LocalSrategy   =require("passport-local");
-var methodOverride =require("method-override");
-var flash          =require('connect-flash');
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var Comment= require("./models/comment");
+var Campground = require("./models/campground");
+var User= require("./models/user");
+var seedDB=require("./seeds");
+var passport=require("passport");
+var LocalSrategy=require("passport-local");
+var methodOverride = require("method-override");
+var flash = require('connect-flash');
+
+
 
 
 var campgroundsRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes"); //or index for all purpose routes
 var commentRoutes = require("./routes/comments");
-app.use("/campgrounds", campgroundsRoutes);
-app.use(indexRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
-
 
 var url =process.env.DATABASEURL|| "mongodb://localhost:27017/yelp_camp";
-// mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
 mongoose.connect(url,
     {
         useNewUrlParser: true,
@@ -32,16 +29,11 @@ mongoose.connect(url,
 }).catch(err => {
     console.log("error: ", err.message);
 });
-console.log(process.env.DATABASEURL);
-
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(flash());
-
-
-// seedDB();
 
 //Passport configuration
 app.use(require("express-session")({
@@ -55,7 +47,7 @@ passport.use(new LocalSrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//IMPORTANT adds current user in every template
+//IMPORTANT adds currentUser,error and success vars in every template
 app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.error = req.flash("error");
@@ -64,14 +56,25 @@ app.use(function (req, res, next) {
 });
 
 
+
+app.use("/campgrounds", campgroundsRoutes);
+app.use(indexRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+
+// seedDB();
+
+
+
 //====================
 //SERVER LISTENING
 //====================
 
-
-let port = process.env.PORT;
-app.set('port',3000||process.env.PORT);
+//
+// let port = process.env.PORT;
+// app.set('port',3000||process.env.PORT);
 
 app.listen(3000,function () {
-    console.log("Server is listeing on port: "+port);
+    console.log("Server is listeing");
 });
